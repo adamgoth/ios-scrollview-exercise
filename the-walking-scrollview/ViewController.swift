5933d239f705bc9ed1e47977beb08d5f50ddc214
Adam Goth <adamgoth@gmail.com>
2016-07-05 21:03:42 -0600

    Adds images and basic functionality to scrollview

--- the-walking-scrollview/ViewController.swift
+++ the-walking-scrollview/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 253
+    let HEIGHT: CGFloat = 384
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for var x = 1; x <= 5; x += 1 {
+            let img = UIImage(named: "\(x)")
+            let imgView = UIImageView(image: img)
+            
+            scrollView.addSubview(imgView)
+            
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(x)), 360, WIDTH, HEIGHT)
+        }
+        
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
